abc4044eac03164012f79af234f7ed3dbcb3badc
Jeff <jtakaki@murad.com>
2017-07-03 20:29:51 -0700

    update webpack config production code

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -19,7 +19,18 @@ var config = {
         loader : 'babel'
       }
     ]
-  }
+  },
+  plugins: [
+        // new HtmlWebpackPlugin({
+        //     filename: 'index.html',
+        //     template: 'index_template.html'
+        // }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ]
 };
 
 module.exports = config;
